794e774a2f0e9d4168c75dc356d8579e8c8f466d
swanjohn99 <bora.india@gmail.com>
2021-09-04 15:07:29 -0400

    Update iasg_hide_orig.js

--- iasg_hide_orig.js
+++ iasg_hide_orig.js
@@ -2,12 +2,9 @@
    // your code here
 
 if(location.hostname.match('internationalsexguide')){
-  [].forEach.call(document.querySelectorAll('.bbcode_container'), function (el) {
+  [].forEach.call(document.querySelectorAll('.bbcode_container,.userinfo,.postfoot'), function (el) {
   el.style.visibility = 'hidden';
 });
    
-    [].forEach.call(document.querySelectorAll('.userinfo'|'.postfoot'), function (el) {
-  el.style.visibility = 'hidden';
-});
 } // will return null if no match is found
 //}, false);
